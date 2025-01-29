f8cfee2758018a2600573170a6eba91c6c551541
Julio Cesar <jccintr@gmail.com>
2025-01-29 01:14:24 -0300

    Criado ClientDTO

--- src/main/java/com/jcsoftware/jspdesafio2/dtos/ClientDTO.java
+++ src/main/java/com/jcsoftware/jspdesafio2/dtos/ClientDTO.java
@@ -1,5 +1,77 @@
 package com.jcsoftware.jspdesafio2.dtos;
 
+import java.time.LocalDate;
+
+import com.jcsoftware.jspdesafio2.entities.Client;
+
 public class ClientDTO {
+
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private LocalDate birthDate;
+	private Integer children;
+
+	public ClientDTO() {
+
+	}
+
+	public ClientDTO(Client c) {
+		this.id = c.getId();
+		this.name = c.getName();
+		this.cpf = c.getCpf();
+		this.income = c.getIncome();
+		this.birthDate = c.getBirthDate();
+		this.children = c.getChildren();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
 
 }
