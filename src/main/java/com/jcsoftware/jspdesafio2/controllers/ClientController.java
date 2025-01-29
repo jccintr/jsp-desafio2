bc116516c68901a953925dfc34b5d6d531032460
Julio Cesar <jccintr@gmail.com>
2025-01-29 15:51:01 -0300

    Criado endpoint get client by id

--- src/main/java/com/jcsoftware/jspdesafio2/controllers/ClientController.java
+++ src/main/java/com/jcsoftware/jspdesafio2/controllers/ClientController.java
@@ -38,6 +38,9 @@ public class ClientController {
 	@GetMapping(value="/{id}")
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
 		
+		ClientDTO clientDTO = service.findById(id);
+		return ResponseEntity.ok().body(clientDTO);
+		
 	}
 	
 	@PostMapping
--- src/main/java/com/jcsoftware/jspdesafio2/services/ClientService.java
+++ src/main/java/com/jcsoftware/jspdesafio2/services/ClientService.java
@@ -1,5 +1,8 @@
 package com.jcsoftware.jspdesafio2.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jcsoftware.jspdesafio2.dtos.ClientDTO;
 import com.jcsoftware.jspdesafio2.entities.Client;
 import com.jcsoftware.jspdesafio2.repositories.ClientRepository;
+import com.jcsoftware.jspdesafio2.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -24,6 +28,12 @@ public class ClientService {
 	
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
+		
+		Optional<Client> clientO = repository.findById(id);
+		Client client = clientO.orElseThrow(() -> new ResourceNotFoundException(id));
+		ClientDTO dto = new ClientDTO();
+		BeanUtils.copyProperties(client, dto);
+		return dto;
 		
 	}
 	
